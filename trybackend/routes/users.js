a142fa788bdff69415925096d8c4f700ee7fd25f
mirbasit01 <mirt11477@gmila.com>
2025-09-22 19:23:17 +0500

    authenticate

--- trybackend/routes/users.js
+++ trybackend/routes/users.js
@@ -1,53 +1,20 @@
-// const mongoose = require('mongoose');
-
-// // const connectDB = async () => {
-// //   try {
-// //     const conn = await mongoose.connect("mongodb://127.0.0.1:27017/yourdbname");
-// //     console.log(`MongoDB connected: ${conn.connection.host}`);
-// //   } catch (error) {
-// //     console.error('MongoDB connection FAILED:', error.message);
-// //     process.exit(1);
-// //   }
-// // };
-
-// mongoose.connect("mongodb://127.0.0.1:27017/yourdbname");
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   nickname: String,
-//   description: String,
-//   category: {
-//     type: Array,
-//     default: []
-//   },
-//   datecreated: {
-//     type: Date,
-//     default: Date.now
-//   }
-// })
-
-// module.exports = mongoose.model('User', userSchema);
-
-// // module.exports = connectDB;
- 
 const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
 // connect to local MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/yourdbname");
+// connect to local MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/yourdbname", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 
-// schema definition
-const userSchema = new mongoose.Schema({
-  name: String,         // simple string
-  nickname: String,     // simple string
-  description: String,  // simple string
-  category: {           // array field
-    type: Array,
-    default: []
-  },
-  datecreated: {        // date field
-    type: Date,
-    default: Date.now   // auto-set current date when user is created
-  }
+ const userSchema = new mongoose.Schema({
+ username: String,
+  secret: String
 });
+userSchema.plugin(plm);
 
 // create a model (collection: "users")
 module.exports = mongoose.model('User', userSchema);
