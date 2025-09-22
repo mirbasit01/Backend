97f4bcf2fe3abd3c0825ad29f82db253a5e2a9d3
mirbasit01 <mirt11477@gmila.com>
2025-09-22 19:23:17 +0500

    authenticate

--- trybackend/routes/index.js
+++ trybackend/routes/index.js
@@ -1,76 +1,118 @@
 // var express = require('express');
 // var router = express.Router();
-// const usermodel = require("./users");
+// const UserModel = require("./users");
+// const passport = require('passport');
+// const localStrategy = require('passport-local');
+
+// passport.use(new localStrategy(UserModel.authenticate()));
 
 // router.get('/', function (req, res) {
 //   res.render('index');
 // });
 
-// router.get('/create', async function (req, res) {
-//   let userdata =  await usermodel.create({
-//     name: 'abdul',
-//     nickname: 'king',
-//     description: 'A software developer',
-//     category: ['nodejs', 'reactjs', 'mongodb' , 'javascript' , 'expressjs' , 'html' , 'css' ],
-//     datecreated: {
-//       type: Date,
-//       default: Date.now()
-//     }
-//   })
-//   res.send(userdata)
-// })
-
-
-
-// router.get('/find', async function (req, res) {
-//   let users = await usermodel.find({
-//     $expr:{
-//      $and:[
-//        {$gt: [{$size: "$category"} ,3]},
-//        {$lt: [{$size: "$category"} ,6]}
-//      ]
-
-//     }
-//   });
-//   res.send(users);
+// router.get('/profile', isLoggedIn, function (req, res) {
+//   res.send("You are logged in as " + req.user.username)
 // });
+
+
+// router.post('/register', async function (req, res) {
+//   let userdata = await UserModel({
+//     username: string,
+//     secret: string
+//   });
+//   UserModel.register(userdata, req.body.password,)
+//     .then(function (registeredUser) {
+//       passport.authenticate("local")(req, res, function () {
+//         res.redirect('/profile');
+//       });
+//     })
+    
+
+// });
+
+// router.post('/login', passport.authenticate('local', {
+//   successRedirect: '/profile',
+//   failureRedirect: '/login'
+// }), function (req, res) {
+//   res.send("You are logged in as " + req.user.username)
+// });
+
+
+
+// router.get('/logout', function (req, res, next) {
+//   req.logout(function(err) {
+//     if (err) { return next(err); }
+//     res.redirect('/');
+//   });
+// });
+
+
+// function isLoggedIn(req, res, next) {
+//   if(req.isAuthenticated()){
+//     return next();
+//   }
+//   res.redirect('/');
+// }
+
 
 // module.exports = router;
 
-var express = require('express');
-var router = express.Router();
- const User = require("./users");
+const express = require('express');
+const router = express.Router();
+const UserModel = require("./users");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+passport.use(new LocalStrategy(UserModel.authenticate()));
+
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
 
 router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/create', async function (req, res) {
-  let userdata = await await User.create({
-  name: "mirbasit01",
-  nickname: "Dev new ",
-  description: "Learning Express and MongoDB",
-  category: ["nodejs", "mongodb" , "expressjs", "javascript", "html", "css"]
-});
-  res.send(userdata);
+router.get('/profile', isLoggedIn, function (req, res) {
+  res.send("You are logged in as " + req.user.username);
 });
 
+router.post('/register', async function (req, res) {
+  try {
+    let userdata = new UserModel({
+      username: req.body.username,
+      secret: req.body.secret
+    });
 
-router.get('/all', async function (req, res) {
-  let users = await User.find();
-  res.send(users);
+    UserModel.register(userdata, req.body.password)
+      .then(function (registeredUser) {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect('/profile');
+        });
+      });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-// router.get('/find', async function (req, res) {
-//   let users = await usermodel.find({
-//     $expr: {
-//       $and: [
-//         { $gt: [{ $size: "$category" }, 3] },
-//         { $lt: [{ $size: "$category" }, 6] }
-//       ]
-//     }
-//   });
-//   res.send(users);
-// });
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+  })
+);
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
 
 module.exports = router;
